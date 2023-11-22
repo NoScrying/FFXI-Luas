4a2850aac936febe7083e2b15fb4fa4e9a9e74ee
NoScrying <42573630+NoScrying@users.noreply.github.com>
2023-11-22 11:46:41 +0100

    Update Noscrying_DNC.lua

--- Noscrying_DNC.lua
+++ Noscrying_DNC.lua
@@ -5,7 +5,7 @@ function get_sets()
 	Melee_Set_Names = {'normal', 'DT'}
 	sets.melee = {} 				-- Leave this empty.
 	sets.melee.normal = {
-	ammo="Ginsen",
+	ammo="Yamarang",
     head="Adhemar Bonnet +1",
     body={ name="Adhemar Jacket +1", augments={'DEX+12','AGI+12','Accuracy+20',}},
     hands={ name="Adhemar Wrist. +1", augments={'DEX+12','AGI+12','Accuracy+20',}},
@@ -17,10 +17,10 @@ function get_sets()
 	right_ear="Cessance Earring",
 	left_ring="Epona\'s Ring",
 	right_ring="Gere Ring",
-	back="Letalis Mantle",
+	back="Sacro Mantle",
 	}
 	sets.melee.DT = {
-	ammo="Ginsen",
+	ammo="Yamarang",
     head="Malignance Chapeau",
     body="Malignance Tabard",
 	hands="Malignance Gloves",
@@ -32,7 +32,7 @@ function get_sets()
 	right_ear="Cessance Earring",
 	left_ring="Epona\'s Ring",
 	right_ring="Defending Ring",
-    back="Letalis Mantle",
+    back="Sacro Mantle",
 	}
 	
 	sets.melee.TH = {
@@ -48,7 +48,7 @@ function get_sets()
 	right_ear="Cessance Earring",
 	left_ring="Epona\'s Ring",
 	right_ring="Ilabrat Ring",
-	back="Letalis Mantle",
+	back="Sacro Mantle",
 	}
 	
 	sets.ranged = {}
@@ -64,7 +64,7 @@ function get_sets()
 	right_ear="Suppanomimi",
     left_ring="Epona's Ring",
     right_ring="Mummu Ring",
-	back="Letalis Mantle",
+	back="Sacro Mantle",
 	}
 	
 	
@@ -72,7 +72,7 @@ function get_sets()
 	sets.ws['Aeolian Edge'] = {
     ammo="Oshasha's Treatise",
     head={ name="Herculean Helm", augments={'Mag. Acc.+20 "Mag.Atk.Bns."+20','Weapon skill damage +5%','STR+9','Mag. Acc.+1',}},
-    body="Rawhide Vest",
+    body="Nyame Mail",
     hands={ name="Herculean Gloves", augments={'"Mag.Atk.Bns."+23','Weapon skill damage +4%','Mag. Acc.+5',}},
     legs={ name="Herculean Trousers", augments={'Mag. Acc.+15 "Mag.Atk.Bns."+15','Weapon skill damage +5%','"Mag.Atk.Bns."+15',}},
     feet={ name="Herculean Boots", augments={'"Mag.Atk.Bns."+23','Weapon skill damage +5%','Mag. Acc.+13',}},
@@ -86,47 +86,48 @@ function get_sets()
 }
 	sets.ws['Rudra\'s Storm'] = {
     ammo="Oshasha's Treatise",
-    head={ name="Blistering Sallet +1", augments={'Path: A',}},
-    body="Malignance Tabard",
-    hands={ name="Herculean Gloves", augments={'"Triple Atk."+3','STR+13',}},
-    legs="Meg. Chausses +2",
-    feet={ name="Herculean Boots", augments={'Accuracy+28','"Triple Atk."+4',}},
+    head={ name="Herculean Helm", augments={'Accuracy+3','AGI+2','Weapon skill damage +7%','Accuracy+18 Attack+18','Mag. Acc.+15 "Mag.Atk.Bns."+15',}},
+    body="Meg. Cuirie +2",
+    hands="Meg. Gloves +2",
+    legs={ name="Lustr. Subligar +1", augments={'Accuracy+20','DEX+8','Crit. hit rate+3%',}},
+    feet={ name="Lustra. Leggings +1", augments={'HP+65','STR+15','DEX+15',}},
     neck="Fotia Gorget",
     waist="Fotia Belt",
-    left_ear="Ishvara Earring",
+    left_ear="Odr Earring",
     right_ear={ name="Moonshade Earring", augments={'"Mag.Atk.Bns."+4','TP Bonus +250',}},
-    left_ring="Epona's Ring",
+    left_ring="Lehko's Ring",
     right_ring="Ilabrat Ring",
     back="Sacro Mantle",
 	}
 	sets.ws['Pyrrhic Kleos'] = {
     ammo="Oshasha's Treatise",
-    head={ name="Blistering Sallet +1", augments={'Path: A',}},
+    head={ name="Herculean Helm", augments={'Accuracy+3','AGI+2','Weapon skill damage +7%','Accuracy+18 Attack+18','Mag. Acc.+15 "Mag.Atk.Bns."+15',}},
     body="Meg. Cuirie +2",
-    hands={ name="Herculean Gloves", augments={'"Triple Atk."+3','STR+13',}},
-    legs="Meg. Chausses +2",
-    feet={ name="Herculean Boots", augments={'Accuracy+28','"Triple Atk."+4',}},
-    neck="Rep. Plat. Medal",
-    waist={ name="Sailfi Belt +1", augments={'Path: A',}},
-    left_ear="Sherida Earring",
+    hands="Meg. Gloves +2",
+    legs={ name="Lustr. Subligar +1", augments={'Accuracy+20','DEX+8','Crit. hit rate+3%',}},
+    feet={ name="Lustra. Leggings +1", augments={'HP+65','STR+15','DEX+15',}},
+    neck="Fotia Gorget",
+    waist="Fotia Belt",
+    left_ear="Odr Earring",
     right_ear={ name="Moonshade Earring", augments={'"Mag.Atk.Bns."+4','TP Bonus +250',}},
-    left_ring="Gere Ring",
+    left_ring="Lehko's Ring",
     right_ring="Ilabrat Ring",
     back="Sacro Mantle",
 	}
 	sets.ws['Evisceration'] = {
-    ammo="Oshasha's Treatise",
-    head={ name="Blistering Sallet +1", augments={'Path: A',}},
-    body="Mummu Jacket +2",
-    hands="Mummu Wrists +2",
-    legs="Mummu Kecks +2",
-    feet="Mummu Gamash. +2",
+    ammo="Aurgelmir Orb",
+    head="Gleti's Mask",
+    body="Gleti's Cuirass",
+	hands="Gleti's Gauntlets",
+    legs="Gleti's Breeches",
+	feet="Gleti's Boots",
+    feet={ name="Lustra. Leggings +1", augments={'HP+65','STR+15','DEX+15',}},
     neck="Fotia Gorget",
     waist="Fotia Belt",
-    left_ear="Ishvara Earring",
+    left_ear="Odr Earring",
     right_ear={ name="Moonshade Earring", augments={'"Mag.Atk.Bns."+4','TP Bonus +250',}},
-    left_ring="Epona's Ring",
-    right_ring="Mummu Ring",
+    left_ring="Lehko's Ring",
+    right_ring="Ilabrat Ring",
     back="Sacro Mantle",
 	}
 	sets.ws['Shark Bite'] = {
@@ -181,12 +182,12 @@ function get_sets()
 		
 	sets.idle = {} 					-- Leave this empty.
 	sets.idle.normal = {
-	ammo="Ginsen",
-    head="Malignance Chapeau",
-    body="Malignance Tabard",
-	hands="Malignance Gloves",
-    legs="Malignance Tights",
-	feet="Malignance Boots",
+	ammo="Yamarang",
+    head="Gleti's Mask",
+    body="Gleti's Cuirass",
+	hands="Gleti's Gauntlets",
+    legs="Gleti's Breeches",
+	feet="Gleti's Boots",
     neck="Elite Royal Collar",
     waist={ name="Sailfi Belt +1", augments={'Path: A',}},
     left_ear="Sherida Earring",
