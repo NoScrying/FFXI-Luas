d74e94de07e7348b1f4a6c9e8a0819efebcc4334
NoScrying <42573630+NoScrying@users.noreply.github.com>
2024-12-17 00:08:46 +0100

    Update Noscrying_SCH.lua

--- Noscrying_SCH.lua
+++ Noscrying_SCH.lua
@@ -1,16 +1,16 @@
 function get_sets()
 	send_command('bind f9 gs c toggle melee set')
 	send_command('bind f10 gs c toggle Run set')
-	send_command('bind f7 gs c toggle weapon set')
-	send_command('bind !f7 gs c toggle Sub_Weapons set') --, ! = ALT
+	send_command('bind f7 gs c toggle Main set')
+	send_command('bind !f7 gs c toggle Sub set')
 	send_command('bind f12 gs c toggle TH set') 
 	send_command('bind !numpad1 gs c toggle Buff set')
 	send_command('bind ^f12 gs c toggle Nuke set') 	--, ^ = CTRL
 	send_command ("input //lua load SCH-hud")
 	
 	Melee_Index = 1
-	Weapon_Index = 1
-	Sub_Weapons_Index = 1
+	Main_Index = 1
+	Sub_Index = 1
 	Run_Index = 1
 	TH_Index = 1
 	Buff_Index = 1
@@ -38,14 +38,14 @@ function get_sets()
     head="Arbatel Bonnet +2",
     body="Arbatel Gown +2",
     hands="Nyame Gauntlets",
-	legs="Arbatel Pants +2",
+    legs="Arbatel Pants +2",
     feet="Nyame Sollerets",
     neck="Warder's Charm +1",
     waist="Carrier's Sash",
-    left_ear="Odnowa Earring +1",
+    left_ear="Eabani Earring",
     right_ear="Regal Earring",
-    left_ring="Defending Ring",
-    right_ring={ name="Dark Ring", augments={'Phys. dmg. taken -6%','Magic dmg. taken -3%',}},
+    left_ring="Shadow Ring",
+    right_ring="Purity Ring",
     back="Moonbeam Cape",
 	}
 	sets.melee.Refresh =  {
@@ -59,8 +59,8 @@ function get_sets()
     waist="Embla Sash",
     left_ear="Crep. Earring",
     right_ear="Savant's Earring",
-    left_ring={name = "Stikini Ring +1", bag = "Wardrobe 4"},
-	right_ring={name = "Stikini Ring +1", bag = "Wardrobe 7"},
+    left_ring={name = "Stikini Ring +1", bag = "Wardrobe 1"},
+	right_ring={name = "Stikini Ring +1", bag = "Wardrobe 2"},
     back="Moonbeam Cape",
 	}
 	sets.melee.DW = {
@@ -93,8 +93,8 @@ function get_sets()
 	waist="Embla Sash",
     left_ear="Savant's Earring",
     right_ear="Regal Earring",
-    left_ring={name = "Stikini Ring +1", bag = "Wardrobe 4"},
-	right_ring={name = "Stikini Ring +1", bag = "Wardrobe 7"},
+    left_ring={name = "Stikini Ring +1", bag = "Wardrobe 1"},
+	right_ring={name = "Stikini Ring +1", bag = "Wardrobe 2"},
     back="Moonbeam Cape",
 	}
 	sets.Run.DT =  {
@@ -108,8 +108,8 @@ function get_sets()
 	waist="Embla Sash",
     left_ear="Infused Earring",
     right_ear="Regal Earring",
-    left_ring={name = "Stikini Ring +1", bag = "Wardrobe 4"},
-	right_ring={name = "Stikini Ring +1", bag = "Wardrobe 7"},
+    left_ring={name = "Stikini Ring +1", bag = "Wardrobe 1"},
+	right_ring={name = "Stikini Ring +1", bag = "Wardrobe 2"},
     back="Moonbeam Cape",
 	}
 	
@@ -123,7 +123,7 @@ function get_sets()
     hands="Nyame Gauntlets",
 	legs="Arbatel Pants +2",
     feet="Nyame Sollerets",
-    neck="Elite Royal Collar",
+    neck="Loricate Torque +1",
     left_ear="Friomisi Earring",
     right_ear="Regal Earring",
     left_ring="Defending Ring",
@@ -131,33 +131,29 @@ function get_sets()
     back={ name="Lugh's Cape", augments={'INT+20','Mag. Acc+20 /Mag. Dmg.+20','"Mag.Atk.Bns."+10','Phys. dmg. taken-10%',}},
 	}
 	
-	Weapon_Set_Names = {'Daybreak',"Bunzi", 'Malignance',"Mpaca"}
-	sets.weapon = {}
-	sets.weapon.Daybreak = {
-    main="Daybreak",
-	sub="Ammurapi Shield",
+	Main_Set_Names = {"Pedagogy Staff","Mpaca","Daybreak"}--Bunzi,  'Malignance'
+	sets.Main = {}
+	sets.Main["Pedagogy Staff"] = {
+    main="Pedagogy Staff",priority=15,
+    sub="Khonsu",priority=19,
 	}
-	sets.weapon.Bunzi = {
-    main="Bunzi's Rod",
-	sub="Ammurapi Shield",
-	}
-	sets.weapon.Malignance = {
-    main="Malignance Pole",
-    sub="Khonsu",
-	}
-	sets.weapon.Mpaca = {
-	main="Mpaca's Staff",
-	sub="Khonsu",
+	sets.Main["Mpaca"] = {
+	main="Mpaca's Staff",priority=19,
+	sub="Khonsu",priority=15,
 	}	
-	Sub_Weapons_Set_Names = {'Mpaca', 'Maxentius'}
-	sets.sub_weapons = {}
-	sets.sub_weapons.Mpaca = {
-	main="Mpaca's Staff",
-	sub="Khonsu",
+	sets.Main["Daybreak"] = {
+    main="Daybreak",priority=19,
+	sub="Ammurapi Shield",priority=15,
+	}	
+	Sub_Set_Names = {"Bunzi's Rod",'Maxentius'}
+	sets.Sub = {}
+	sets.Sub["Bunzi's Rod"] = {
+    main="Bunzi's Rod",priority=19,
+	sub="Ammurapi Shield",priority=15,
 	}
-	sets.sub_weapons.Maxentius = {
-    main="Maxentius",
-	sub="Ammurapi Shield",
+	sets.Sub["Maxentius"] = {
+    main="Maxentius",priority=19,
+	sub="Ammurapi Shield",priority=15,
 	}
 	
 	sets.ws = {}
@@ -252,8 +248,8 @@ function get_sets()
     neck="Sibyl Scarf",
     left_ear="Malignance Earring",
     right_ear="Arbatel Earring +1",
-    left_ring="Stikini Ring +1",
-    right_ring="Stikini Ring +1",
+    left_ring={name = "Stikini Ring +1", bag = "Wardrobe 2"},
+    right_ring={name = "Stikini Ring +1", bag = "Wardrobe 1"},
     back={ name="Lugh's Cape", augments={'INT+20','Mag. Acc+20 /Mag. Dmg.+20','"Mag.Atk.Bns."+10','Phys. dmg. taken-10%',}},
 
 	}
@@ -269,6 +265,10 @@ function get_sets()
 	hands="Arbatel Bracers +2",
     legs={ name="Telchine Braconi", augments={'"Cure" potency +8%','Enh. Mag. eff. dur. +10',}},
     feet={ name="Telchine Pigaches", augments={'Enh. Mag. eff. dur. +10',}},
+	}
+	sets.ja['Accession'] = {
+	main="Pedagogy Staff",
+	sub="Khonsu",
 	}
 	sets.ja['Penury'] = {
 	legs="Arbatel Pants +2",
@@ -289,15 +289,15 @@ function get_sets()
 	waist="Embla Sash",
     left_ear="Infused Earring",
     right_ear="Malignance Earring",
-    left_ring="Stikini Ring +1",
-    right_ring="Stikini Ring +1",
+    left_ring={name = "Stikini Ring +1", bag = "Wardrobe 2"},
+    right_ring={name = "Stikini Ring +1", bag = "Wardrobe 1"},
     back="Moonbeam Cape",
 	}
 	
 	sets.precast = {} 
 	sets.precast.fastcast = {	--78
     ammo="Sapience Orb", 		--2
-    head="Amalric Coif",		--10
+    head="Amalric Coif +1",		--10
     body="Pinga Tunic",			--13
     hands="Pinga Mittens",		--5
     legs="Pinga Pants",			--11
@@ -362,86 +362,100 @@ function get_sets()
 	
 	
 	sets.midcast.enhancingduration = {
-	main="Pedagogy Staff",
-	sub="Khonsu",
-	ammo="Impatiens",
+	main="Pedagogy Staff",priority=15,
+	sub="Khonsu",priority=10,
+    ammo="Staunch Tathlum +1",
 	head={ name="Telchine Cap", augments={'"Cure" potency +8%','Enh. Mag. eff. dur. +10',}},
     body={ name="Telchine Chas.", augments={'"Cure" potency +7%','Enh. Mag. eff. dur. +10',}},
     hands={ name="Telchine Gloves", augments={'"Cure" potency +7%','Enh. Mag. eff. dur. +9',}},
      legs={ name="Telchine Braconi", augments={'"Cure" potency +8%','Enh. Mag. eff. dur. +10',}},
     feet={ name="Telchine Pigaches", augments={'Enh. Mag. eff. dur. +10',}},
 	waist="Embla Sash",
-    neck="Sibyl Scarf",
+    neck="Incanter's Torque",
     left_ear="Malignance Earring",
     right_ear="Arbatel Earring +1",
-    left_ring="Stikini Ring +1",
-    right_ring="Stikini Ring +1",
+    left_ring={name = "Stikini Ring +1", bag = "Wardrobe 2"},
+    right_ring={name = "Stikini Ring +1", bag = "Wardrobe 1"},
     back={ name="Lugh's Cape", augments={'INT+20','Mag. Acc+20 /Mag. Dmg.+20','"Mag.Atk.Bns."+10','Phys. dmg. taken-10%',}},
 	}
 	sets.midcast.enhancingskill = {
-	main="Pedagogy Staff",
-	sub="Khonsu",
-    ammo="Impatiens",
+	main="Pedagogy Staff",priority=15,
+	sub="Khonsu",priority=10,
+    ammo="Staunch Tathlum +1",
     head="Arbatel Bonnet +2",
     body="Arbatel Gown +2",
     hands="Chironic Gloves",
     legs={ name="Telchine Braconi", augments={'"Cure" potency +8%','Enh. Mag. eff. dur. +10',}},
     feet={ name="Telchine Pigaches", augments={'Enh. Mag. eff. dur. +10',}},
-    neck="Melic Torque",
+    neck="Incanter's Torque",
     waist="Embla Sash",
     left_ear="Mimir Earring",
-    right_ear="Augment. Earring",
-    left_ring="Stikini Ring +1",
-    right_ring="Stikini Ring +1",
+    right_ear="Andoaa Earring",
+    left_ring={name = "Stikini Ring +1", bag = "Wardrobe 2"},
+    right_ring={name = "Stikini Ring +1", bag = "Wardrobe 1"},
     back={ name="Aurist's Cape +1", augments={'Path: A',}},
 	}	
  	sets.midcast.regen = {
-	main="Pedagogy Staff",
-	sub="Khonsu",
-	ammo="Impatiens",
+	main="Pedagogy Staff",priority=15,
+	sub="Khonsu",priority=10,
+    ammo="Staunch Tathlum +1",
     head="Arbatel Bonnet +2",
     body={ name="Telchine Chas.", augments={'"Regen" potency+3',}},
     hands={ name="Telchine Gloves", augments={'"Regen" potency+3',}},
     legs={ name="Telchine Braconi", augments={'"Cure" potency +8%','Enh. Mag. eff. dur. +10',}},
     feet={ name="Telchine Pigaches", augments={'Enh. Mag. eff. dur. +10',}},
-    neck="Willpower Torque",
+    neck="Incanter's Torque",
     waist="Embla Sash",
     left_ear="Magnetic Earring",
     right_ear="Halasz Earring",
-    left_ring="Stikini Ring +1",
-    right_ring="Evanescence Ring",
+    left_ring={name = "Stikini Ring +1", bag = "Wardrobe 2"},
+    right_ring={ name="Mephitas's Ring +1", augments={'Path: A',}},
     back={ name="Bookworm's Cape", augments={'INT+3','MND+2','Helix eff. dur. +20','"Regen" potency+9',}},
 	}	
 	
 	sets.midcast.refresh = {
     ammo="Impatiens",
-	head="Amalric Coif",
+	head="Amalric Coif +1",
     body={ name="Telchine Chas.", augments={'"Cure" potency +7%','Enh. Mag. eff. dur. +10',}},
     hands={ name="Telchine Gloves", augments={'"Cure" potency +7%','Enh. Mag. eff. dur. +9',}},
      legs={ name="Telchine Braconi", augments={'"Cure" potency +8%','Enh. Mag. eff. dur. +10',}},
     feet={ name="Telchine Pigaches", augments={'Enh. Mag. eff. dur. +10',}},
-    neck="Sibyl Scarf",
+    neck="Incanter's Torque",
 	waist="Gishdubar Sash",
     left_ear="Magnetic Earring",
     right_ear="Halasz Earring",
-    left_ring="Stikini Ring +1",
+    left_ring={name = "Stikini Ring +1", bag = "Wardrobe 2"},
     right_ring="Evanescence Ring",
     back={ name="Lugh's Cape", augments={'INT+20','Mag. Acc+20 /Mag. Dmg.+20','"Fast Cast"+10',}},
 	}
-	
+	sets.midcast.Aquaveil = {
+    ammo="Impatiens",
+	head="Amalric Coif +1",
+    body={ name="Telchine Chas.", augments={'"Cure" potency +7%','Enh. Mag. eff. dur. +10',}},
+    hands={ name="Telchine Gloves", augments={'"Cure" potency +7%','Enh. Mag. eff. dur. +9',}},
+     legs={ name="Telchine Braconi", augments={'"Cure" potency +8%','Enh. Mag. eff. dur. +10',}},
+    feet={ name="Telchine Pigaches", augments={'Enh. Mag. eff. dur. +10',}},
+    neck="Incanter's Torque",
+	waist="Emphatikos Rope",
+    left_ear="Magnetic Earring",
+    right_ear="Halasz Earring",
+    left_ring={name = "Stikini Ring +1", bag = "Wardrobe 2"},
+    right_ring="Evanescence Ring",
+    back={ name="Lugh's Cape", augments={'INT+20','Mag. Acc+20 /Mag. Dmg.+20','"Fast Cast"+10',}},
+	}	
 	sets.midcast.cure = {
     ammo="Staunch Tathlum +1",
-    head="Arbatel Bonnet +2",
+    head={ name="Vanya Hood", augments={'MND+10','Spell interruption rate down +15%','"Conserve MP"+6',}},
     body="Pinga Tunic",
     hands={ name="Telchine Gloves", augments={'"Cure" potency +7%','Enh. Mag. eff. dur. +9',}},
-    legs="Arbatel Pants +2",
+    legs={ name="Vanya Slops", augments={'MND+10','Spell interruption rate down +15%','"Conserve MP"+6',}},
     feet={ name="Kaykaus Boots", augments={'Mag. Acc.+15','"Cure" potency +5%','"Fast Cast"+3',}},
-    neck="Phalaina Locket",
-    waist="Gishdubar Sash",
+    neck="Incanter's Torque",
+    waist={ name="Shinjutsu-no-Obi +1", augments={'Path: A',}},
     left_ear="Magnetic Earring",
     right_ear="Halasz Earring",
     left_ring="Defending Ring",
-    right_ring="Freke Ring",
+    right_ring={ name="Mephitas's Ring +1", augments={'Path: A',}},
     back="Moonbeam Cape",
 	}
 
@@ -538,8 +552,9 @@ function get_sets()
 	ElementalGear = {}
 	ElementalGear.Obi = "Hachirin-no-Obi"
 	ElementalGear.Cape = "Twilight Cape"
+	ElementalGear.Feet = "Arbatel Loafers +2"
 	sets.midcast.CureWithLightWeather = {back=ElementalGear.Cape,waist=ElementalGear.Obi}
-	sets.midcast.NukeWithMatchingWeather = {back=ElementalGear.Cape,waist=ElementalGear.Obi}
+	sets.midcast.NukeWithMatchingWeather = {back=ElementalGear.Cape,waist=ElementalGear.Obi,feet=ElementalGear.Feet}
 	
 	
 	Buff_Set_Names = {'Holywater'}
@@ -549,7 +564,7 @@ function get_sets()
 	}
 	sets.buff.Holywater = {
     neck="Nicander's Necklace",
-    left_ring="Blenmot's Ring",
+    left_ring="Blenmot's Ring +1",
     right_ring="Purity Ring",
 	}
 	sets.buff.CursnaOthers = {
@@ -591,65 +606,39 @@ function midcast(spell)
 				equip(sets.ja['Rapture'])
 		end
 	end
-	if spell.name:match('Refresh') then
-		equip(sets.midcast.refresh)
-	end
 	if spell.name:match('Regen') then
-		equip(sets.midcast.regen) else
-			if spell.name:match('Refresh') or ('Aquaveil') then
-				equip(sets.midcast.refresh) else
-					if spell.name:match("Phalanx") or ("Bar") then
-						equip(sets.midcast.enhancingskill)
-					if spell.skill == 'Enhancing Magic'  then
-						equip(sets.midcast.enhancingduration)
-
+		equip(sets.midcast.regen) else	
+			if T{"Aquaveil","Refresh"}:contains(spell.name) then
+				equip(sets.midcast.Aquaveil) else
+					if T{"Refresh"}:contains(spell.name) then
+						equip(sets.midcast.Refresh) else
+							if  T{"Phalanx","Bar"}:contains(spell.name) then
+								equip(sets.midcast.enhancingskill) else
+							if spell.skill == 'Enhancing Magic'  then
+								equip(sets.midcast.enhancingduration)
+						end
 					end
 				end
 			end
 		end
-		
 	if spell.name:match('Klimaform') then
 		equip(sets.ja['Klimaform'])
 	end
-	if spell.name:match("Sleep") or ("Break") or ("Dispel") or ("Silence") or ("Bio")then 
-		equip (sets.midcast.MACC) else
-			if spell.name:match('Aspir') or ('Drain') then
-				equip (sets.midcast.Drain) else
-			if spell.skill == 'Enfeebling Magic' then
-				equip(sets.midcast.enfeebling)
-			end
-		end
+	if T{"Aspir","Aspir II","Aspir III","Drain"}:contains(spell.name) then
+		equip (sets.midcast.Drain)
 	end
-        -- if spell.skill == 'Elemental Magic' or spell.name:match ("Burn") or ("Choke") or ("Shock") then
-			-- equip(sets.Nuke.Nukes)
-            -- if world.weather_element == spell.element or world.day_element == spell.element then
-                -- equip(sets.midcast.NukeWithMatchingWeather)
-			-- end
-		-- end
-		-- if spell.english:contains('helix II') or spell.english:contains('IV') or spell.english:contains('V') then
-			-- if player.equipment.main == "Bunzi's Rod" then
-				-- equip(sets.Nuke["MB - With Bunzi"])
-				    -- if world.weather_element == spell.element or world.day_element == spell.element then
-						-- equip(sets.midcast.NukeWithMatchingWeather)
-					-- end
-				-- end
-			-- end
-		-- if spell.english:contains('helix II') or spell.english:contains('IV') or spell.english:contains('V') then
-			-- if player.equipment.main ~= "Bunzi's Rod" then
-				-- equip(sets.Nuke["MB - Without Bunzi"])
-				    -- if world.weather_element == spell.element or world.day_element == spell.element then
-						-- equip(sets.midcast.NukeWithMatchingWeather)
-					-- end
-				-- end
-			-- end
-		if spell.action_type == 'Magic' then
-        if spell.skill == 'Elemental Magic' then
-            equip(sets.Nuke[Nuke_Set_Names[Nuke_Index]])
-            if world.weather_element == spell.element or world.day_element == spell.element then --, If spell matches day or zone weather, then adds below set to above set
-                equip(sets.midcast.NukeWithMatchingWeather)
-		end
+	if spell.skill == 'Enfeebling Magic' then
+		equip(sets.midcast.enfeebling) else
+	if T{"Sleep","Sleep II","Sleepga","Sleepga II","Break","Bio","Bind","Blind","Frazzle","Dispel","Silence"}:contains(spell.name) then
+		equip (sets.midcast.MACC)
 	end
 end
+    if spell.skill == 'Elemental Magic' then
+        equip(sets.Nuke[Nuke_Set_Names[Nuke_Index]])
+            if world.weather_element == spell.element or world.day_element == spell.element then --, If spell matches day or zone weather, then adds below set to above set
+                equip(sets.midcast.NukeWithMatchingWeather)
+		end	
+	end
 		if spell.name:match('Noctohelix') then 
 			equip(sets.Nuke["Noctohelix"])
 		end
@@ -678,16 +667,16 @@ end
 
 function aftercast(spell)
 	idle()
-	equip(sets.weapon[Weapon_Set_Names[Weapon_Index]])
+	equip(sets.Main[Main_Set_Names[Main_Index]])
 end
  
 function buff_change(buff,gain)
     if buff == 'Perpetuance' then
         if gain then
-            equip(	sets.ja['Perpetuance'])
-            disable("hands","Legs","Feet")
+            equip(sets.ja['Perpetuance'])
+            disable("main","sub","hands","Legs","Feet")
         else
-            enable("hands","Legs","Feet")
+            enable("main","sub","hands","Legs","Feet")
             idle()
         end
     end
@@ -723,18 +712,18 @@ function self_command(command)
         windower.add_to_chat('Movement mode is now: '..Run_Set_Names[Run_Index])
 		equip(sets.Run[Run_Set_Names[Run_Index]])
 	end
-	if command == 'toggle weapon set' then
-        Weapon_Index = Weapon_Index +1
-    if Weapon_Index > #Weapon_Set_Names then Weapon_Index = 1 end
-        windower.add_to_chat('Weapon is now: '..Weapon_Set_Names[Weapon_Index])
-        equip(sets.weapon[Weapon_Set_Names[Weapon_Index]])
+	if command == 'toggle Main set' then
+        Main_Index = Main_Index +1
+    if Main_Index > #Main_Set_Names then Main_Index = 1 end
+        windower.add_to_chat('Main Weapon is now: '..Main_Set_Names[Main_Index])
+        equip(sets.Main[Main_Set_Names[Main_Index]])
     end
-	if command == 'toggle Sub_Weapons set' then
-        Sub_Weapons_Index = Sub_Weapons_Index +1
-        if Sub_Weapons_Index > #Sub_Weapons_Set_Names then Sub_Weapons_Index = 1 end
-        windower.add_to_chat('Sub Weapon is now: '..Sub_Weapons_Set_Names[Sub_Weapons_Index])
-		equip(sets.sub_weapons[Sub_Weapons_Set_Names[Sub_Weapons_Index]])
-	end
+	if command == 'toggle Sub set' then
+        Sub_Index = Sub_Index +1
+    if Sub_Index > #Sub_Set_Names then Sub_Index = 1 end
+        windower.add_to_chat('Sub Weapon is now: '..Sub_Set_Names[Sub_Index])
+        equip(sets.Sub[Sub_Set_Names[Sub_Index]])
+    end
 	if command == 'toggle TH set' then
         TH_Index = TH_Index +1
     if TH_Index > #TH_Set_Names then TH_Index = 1 end
@@ -755,18 +744,33 @@ function self_command(command)
 end
 
 function file_unload() --, Unbinds defined keybinds when changing jobs, can also use "send_command('clearbinds')" to wipe any and all
+send_command('unbind f7')
+send_command('unbind !f7')
+send_command('unbind ^f7')
+
 send_command('unbind f9')
 send_command('unbind !f9')
 send_command('unbind ^f9')
+
 send_command('unbind f10')
 send_command('unbind !f10')
+send_command('unbind ^f10')
+
 send_command('unbind f12')
 send_command('unbind !f12')
-send_command('unbind f7')
-send_command('unbind !f7')
-send_command('unbind !numpad1')
-send_command('unbind ^numpad1')
-send_command('unbind !numpad0')
-send_command('unbind !numpad7')
+send_command('unbind ^f12')
+
+send_command('unbind Numpad1')
+send_command('unbind !Numpad1')
+send_command('unbind ^Numpad1')
+
+send_command('unbind Numpad3')
+send_command('unbind !Numpad3')
+send_command('unbind ^Numpad3')
+
+send_command('unbind Numpad0')
+send_command('unbind !Numpad0')
+send_command('unbind ^Numpad0')
+send_command('unbind Numpad0')
 send_command ("input //lua u SCH-hud")
 end
